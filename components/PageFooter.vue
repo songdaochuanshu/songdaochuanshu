6d15679e234ae540a8bf71caedd87243db2743d6
松岛川树 <songdaochuanshu@gmail.com>
2026-06-26 22:52:19 +0800

    feat: 新增 PageFooter 统一页脚组件

---
+++ components/PageFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="border-t border-gray-100 dark:border-gray-800 mt-16">
+    <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <p class="text-center text-xs text-gray-400 dark:text-gray-500">© 2026 松岛川树</p>
+    </div>
+  </footer>
+</template>
